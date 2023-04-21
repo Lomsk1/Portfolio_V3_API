1fed4559499649d6db02d64499cb7276d9cc77a3
Lomsk1 <lomsianidzegiorgi123@gmail.com>
2023-04-21 13:00:38 +0400

    factory controllers

--- src/controllers/handlerFactory.ts
+++ src/controllers/handlerFactory.ts
@@ -19,24 +19,6 @@ export const getAll = (Model: any) =>
     });
   });
 
-export const getOne = (Model) =>
-  catchAsync(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-
-    const doc = await query;
-
-    if (!doc) {
-      return next(new AppError("No document found with what ID", 404));
-    }
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        data: doc,
-      },
-    });
-  });
-
 export const createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
@@ -45,5 +27,37 @@ export const createOne = (Model) =>
       data: {
         doc,
       },
+    });
+  });
+
+export const updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        doc,
+      },
+    });
+  });
+
+export const deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+    res.status(204).json({
+      status: "success",
+      data: null,
     });
   });
