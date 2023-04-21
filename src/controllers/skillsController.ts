105f0f6c44d73f8550a1baeea96afb138c24cdde
Lomsk1 <lomsianidzegiorgi123@gmail.com>
2023-04-21 13:08:20 +0400

    skills

--- src/controllers/skillsController.ts
+++ src/controllers/skillsController.ts
@@ -1,5 +1,7 @@
 import Skills from "../models/skillsModel";
-import { createOne, getAll } from "./handlerFactory";
+import { createOne, deleteOne, getAll, updateOne } from "./handlerFactory";
 
 export const getAllSkills = getAll(Skills);
 export const createSkills = createOne(Skills);
+export const updateSkills = updateOne(Skills);
+export const deleteSkills = deleteOne(Skills);
--- src/routes/skillsRoute.ts
+++ src/routes/skillsRoute.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { createSkills, getAllSkills } from "../controllers/skillsController";
+import {
+  createSkills,
+  deleteSkills,
+  getAllSkills,
+  updateSkills,
+} from "../controllers/skillsController";
 
 const skillRouter = express.Router({
   mergeParams: true,
 });
 
 skillRouter.route("/").get(getAllSkills).post(createSkills);
+skillRouter.route("/:id").patch(updateSkills).delete(deleteSkills);
 
 export default skillRouter;
